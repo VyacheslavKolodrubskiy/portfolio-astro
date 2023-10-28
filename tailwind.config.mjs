e3d9b5c0d6f8f51dfa27d8275e8cc40c36fae50a
slava <vyacheslavkolodrubskiy@gmail.com>
2023-10-28 11:31:34 +0600

    1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,14 @@ export default {
 			container: {
 				center: true,
 				padding: '1rem',
+				screens: {
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1280px',
+					'2xl': '1536px',
+					'3xl': '1920px',
+				}
 			}
 		},
 	},
